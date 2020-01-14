253f4d4e4cb89616a50ea35886ca345c5d7c46d7
John Doeirma <irmaaa96@hotmail.com>
2020-01-14 21:26:09 +0100

    done som change in js css

--- javascript/app.js
+++ javascript/app.js
@@ -2,5 +2,12 @@ const button = document.querySelector("#button");
 const nameEl = document.querySelector("#name");
 const inputCount = document.querySelector("#country");
  
+button.addEventListener('click', async function() {
  
-button.addEventListener('click', async function() 
+    
+    const inputText1 = nameEl.value;
+    const inputText2 = inputCount.value;
+    
+   
+   API(inputText1, inputText2)
+       
