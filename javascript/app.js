a59db3804ea2cb120d8ca24677a8c09812d370ac
John Doeirma <irmaaa96@hotmail.com>
2020-01-15 00:12:56 +0100

    added some stuff in my app.js

--- javascript/app.js
+++ javascript/app.js
@@ -9,5 +9,21 @@ button.addEventListener('click', async function() {
     const inputText2 = inputCount.value;
     
    
-   API(inputText1, inputText2)
-       
+   API(inputText1, inputText2)   
+   .then(data => {
+               const resultHtml = document.querySelector("#resultHtml");
+               let html = ""
+               
+               data.results.forEach((result) => {
+                    
+                
+                    html += `<p>${result.name}</p>
+                    <p>${result.day} Day</p>
+                    <p>${result.month} Month</p>
+                    `;
+               
+               });
+               resultHtml.innerHTML = html;
+           }) 
+    });
+    
