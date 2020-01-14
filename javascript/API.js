e2840b6acad85f97039d1a107d69c0bec319a424
John Doeirma <irmaaa96@hotmail.com>
2020-01-14 17:42:57 +0100

    some change

--- javascript/API.js
+++ javascript/API.js
@@ -1,4 +1,6 @@
 
+    const API = async (inputText1, inputText2) => {
+
     const response = await fetch(`https://api.abalin.net/getdate?name=${inputText1}&country=${inputText2}`);
     const data = await response.json();
     console.log(data);
