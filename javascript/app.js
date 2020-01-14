49b48ed334385d1d2f513063c7f24b3ba1e02e54
John Doeirma <irmaaa96@hotmail.com>
2020-01-14 17:59:41 +0100

    add a new app

---
+++ javascript/app.js
@@ -0,0 +1,6 @@
+const button = document.querySelector("#button");
+const nameEl = document.querySelector("#name");
+const inputCount = document.querySelector("#country");
+ 
+ 
+button.addEventListener('click', async function() 
