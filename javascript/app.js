bdc789b4dc9f82a1215928df052ca846b82d03ff
John Doeirma <irmaaa96@hotmail.com>
2020-01-16 17:50:52 +0100

    done

--- javascript/app.js
+++ javascript/app.js
@@ -5,27 +5,26 @@ const inputCount = document.querySelector("#country");
 
  
   button1.addEventListener('click', function() {
- 
-    
     const inputText1 = nameEl.value;
     const inputText2 = inputCount.value;
 
-
    API (inputText1, inputText2, )   
    .then(searchResult => {
-    const resultHtml = document.querySelector("#resultHtml");
-    let html = ""
-    searchResult .results.forEach((data) => {
+        const resultHtml = document.querySelector("#resultHtml");
+        let html = ""
+        searchResult .results.forEach((data) => {
                     
-    html += `<p>${data.name}</p>
-    <p>${data.day} Day</p>
-    <p>${data.month} Month</p>
+    html += `
+   <p>${data.name}</p>
+   <p>${data.day} Day</p>
+   <p>${data.month} Month</p>
       `;
                
        });
        resultHtml.innerHTML = html;
      }) 
  });
+
 
  const inputMonth = document.querySelector("#month");
  const inputDay = document.querySelector("#day");
@@ -34,7 +33,7 @@ const inputCount = document.querySelector("#country");
 
  
     button2.addEventListener('click',function() {
-        
+    
     const inputText3 = inputCount2.value;
     const inputText4 = inputMonth.value;
     const inputText5 = inputDay.value;
@@ -46,9 +45,9 @@ const inputCount = document.querySelector("#country");
        
     const apiCon = inputCount2.value;
                 
-        output = `<p>${dataa.dates.day} Day</p>
-               <p>${dataa.dates.month} Month</p>
-               <p>${dataa.namedays[apiCon]}</p>
+    output = `<p>${dataa.dates.day} Day</p>
+              <p>${dataa.dates.month} Month</p>
+              <p>${dataa.namedays[apiCon]}</p>
             `;
                
        });
