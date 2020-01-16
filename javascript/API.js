55060b0a0ca4a797e00a3f1a16e393ec0cb0309b
John Doeirma <irmaaa96@hotmail.com>
2020-01-16 17:11:34 +0100

    Now i am finish

--- javascript/API.js
+++ javascript/API.js
@@ -2,6 +2,16 @@
     const API = async (inputText1, inputText2) => {
 
     const response = await fetch(`https://api.abalin.net/getdate?name=${inputText1}&country=${inputText2}`);
+    const searchResult = await response.json();
+    console.log(searchResult);
+    return searchResult;
+};
+
+
+
+const api2 = async (inputText3, inputText4, inputText5) => {
+
+    const response = await fetch(`https://api.abalin.net/namedays?country=${inputText3}&month=${inputText4}&day=${inputText5}`);
     const data = await response.json();
     console.log(data);
  
--- javascript/app.js
+++ javascript/app.js
@@ -1,29 +1,60 @@
-const button = document.querySelector("#button");
+const resultHtml = document.querySelector("#resultHtml")
+const button1 = document.querySelector("#button1");
 const nameEl = document.querySelector("#name");
 const inputCount = document.querySelector("#country");
+
  
-button.addEventListener('click', async function() {
+  button1.addEventListener('click', function() {
  
     
     const inputText1 = nameEl.value;
     const inputText2 = inputCount.value;
-    
-   
-   API(inputText1, inputText2)   
-   .then(data => {
-               const resultHtml = document.querySelector("#resultHtml");
-               let html = ""
-               
-               data.results.forEach((result) => {
+
+
+   API (inputText1, inputText2, )   
+   .then(searchResult => {
+    const resultHtml = document.querySelector("#resultHtml");
+    let html = ""
+    searchResult .results.forEach((data) => {
                     
-                
-                    html += `<p>${result.name}</p>
-                    <p>${result.day} Day</p>
-                    <p>${result.month} Month</p>
-                    `;
+    html += `<p>${data.name}</p>
+    <p>${data.day} Day</p>
+    <p>${data.month} Month</p>
+      `;
                
-               });
-               resultHtml.innerHTML = html;
-           }) 
+       });
+       resultHtml.innerHTML = html;
+     }) 
+ });
+
+ const inputMonth = document.querySelector("#month");
+ const inputDay = document.querySelector("#day");
+ const inputCount2 = document.querySelector("#country2");
+ const button2 = document.querySelector("#button2");
+
+ 
+    button2.addEventListener('click',function() {
+        
+    const inputText3 = inputCount2.value;
+    const inputText4 = inputMonth.value;
+    const inputText5 = inputDay.value;
+
+    api2 (inputText3, inputText4, inputText5, )   
+   .then(data => {
+    let output = ""
+    data.data.forEach((dataa) =>{
+       
+    const apiCon = inputCount2.value;
+                
+        output = `<p>${dataa.dates.day} Day</p>
+               <p>${dataa.dates.month} Month</p>
+               <p>${dataa.namedays[apiCon]}</p>
+            `;
+               
+       });
+     resultHtml.innerHTML =output;
     });
+ });
     
+    
+  
